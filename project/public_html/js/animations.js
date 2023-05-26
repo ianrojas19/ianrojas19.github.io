4bdefb0ca363d82decfbc64fa420a13a746f2c6e
ianrs_19 <ian.rsq@gmail.com>
2023-05-26 09:38:50 -0600

    Animaciones pro

--- project/public_html/js/animations.js
+++ project/public_html/js/animations.js
@@ -1,13 +1,49 @@
-var checkbox = document.getElementById('checkbox');
-var navigation = document.getElementById('ul-navigation');
-const enable = () => {
+let checkbox = document.getElementById('checkbox');
+let navigation = document.getElementById('ul-navigation');
+let dropProd = document.getElementById('dropdownMenuButton');
+let spanProd = document.getElementById('icon-drop-prod');
+let dropItemDown = document.getElementById('navi-item-down');
+let typeProdMenu = document.getElementById('prod-types-menu');
+let arrow = document.getElementById('arrow');
+
+const enableNav = () => {
     if (checkbox.checked) {
         navigation.classList.remove('disable');
         navigation.classList.add('enable');
+        
     } else {
         navigation.classList.add('disable');
         navigation.classList.remove('enable');
+        navigation.style.height = '250px';
+        typeProdMenu.style.display = 'none';
+        navigation.style.justifyContent = 'flex-start';
+        arrow.style.transform = 'rotateX(0deg)';
+        arrow.style.stroke = '#2764e8e9';
+        spanProd.classList.add('inactive-prod-list');
+        spanProd.classList.remove('active-prod-list');
     }
 }
 
-checkbox.addEventListener('click', enable);
+const enableProdList = () => {
+    if (spanProd.className == 'inactive-prod-list') {
+        navigation.style.height = '550px';
+        typeProdMenu.style.display = 'flex';
+        navigation.style.justifyContent = 'flex-start';
+        arrow.style.transform = 'rotateX(180deg)';
+        arrow.style.stroke = 'red';
+        spanProd.classList.add('active-prod-list');
+        spanProd.classList.remove('inactive-prod-list');
+    }
+    else {
+        navigation.style.height = '250px';
+        typeProdMenu.style.display = 'none';
+        navigation.style.justifyContent = 'flex-start';
+        arrow.style.transform = 'rotateX(0deg)';
+        arrow.style.stroke = '#2764e8e9';
+        spanProd.classList.add('inactive-prod-list');
+        spanProd.classList.remove('active-prod-list');
+    }
+}
+
+checkbox.addEventListener('click', enableNav);
+dropProd.addEventListener('click', enableProdList);
