c1e97459529cd2abbd5725cfba598b16874eef20
ianrs_19 <ian.rsq@gmail.com>
2023-05-27 15:06:03 -0600

    js

--- project/public_html/js/animations.js
+++ project/public_html/js/animations.js
@@ -1,63 +1,61 @@
-let checkbox = document.getElementById('checkbox');
-let navigation = document.getElementById('ul-navigation');
-let dropProd = document.getElementById('dropdownMenuButton');
-let spanProd = document.getElementById('icon-drop-prod');
-let dropItemDown = document.getElementById('navi-item-down');
-let typeProdMenu = document.getElementById('prod-types-menu');
-let arrow = document.getElementById('arrow');
+const checkbox = document.getElementById('checkbox');
+const navigation = document.getElementById('ul-navigation');
+const dropProd = document.getElementById('dropdownMenuButton');
+const spanProd = document.getElementById('icon-drop-prod');
+const dropItemDown = document.getElementById('navi-item-down');
+const typeProdMenu = document.getElementById('prod-types-menu');
+const arrow = document.getElementById('arrow');
+const link = document.querySelector('.navi-item');
+const link2 = document.querySelector('.dropdown-item');
+
+const closeNav = () => {
+  navigation.classList.add('disable');
+  navigation.classList.remove('enable');
+  checkbox.checked = false;
+};
 
 const enableNav = () => {
-    if (checkbox.checked) {
-        navigation.classList.remove('disable');
-        navigation.classList.add('enable');
-        
-    } else {
-        navigation.classList.add('disable');
-        navigation.classList.remove('enable');
-        navigation.style.height = '260px';
-        typeProdMenu.style.display = 'none';
-        navigation.style.justifyContent = 'flex-start';
-        arrow.style.transform = 'rotateX(0deg)';
-        arrow.style.stroke = '#2764e8e9';
-        dropItemDown.style.marginTop= '0px';
-        spanProd.classList.add('inactive-prod-list');
-        spanProd.classList.remove('active-prod-list');
-    }
-}
+  const enable = checkbox.checked;
+  navigation.classList.toggle('disable', !enable);
+  navigation.classList.toggle('enable', enable);
+
+  if (!enable) {
+    navigation.style.height = '260px';
+    typeProdMenu.style.display = 'none';
+    navigation.style.justifyContent = 'flex-start';
+    arrow.style.transform = 'rotateX(0deg)';
+    arrow.style.stroke = '#2764e8e9';
+    dropItemDown.style.marginTop = '0px';
+    spanProd.classList.add('inactive-prod-list');
+    spanProd.classList.remove('active-prod-list');
+  }
+};
 
 const enableProdList = () => {
-    if (spanProd.className == 'inactive-prod-list') {
-        navigation.style.height = '530px';
-        typeProdMenu.style.display = 'flex';
-        typeProdMenu.style.clipPath = 'polygon(0 0, 100% 0, 100% 100%, 0 100%); !important'
-        navigation.style.justifyContent = 'flex-start';
-        dropItemDown.style.marginTop= '5px';
-        arrow.style.transform = 'rotateX(180deg)';
-        arrow.style.stroke = 'red';
-        spanProd.classList.add('active-prod-list');
-        spanProd.classList.remove('inactive-prod-list');
-    }
-    else {
-        navigation.style.height = '250px';
-        typeProdMenu.style.display = 'none';
-        dropItemDown.style.marginTop= '0px';
-        navigation.style.justifyContent = 'flex-start';
-        arrow.style.transform = 'rotateX(0deg)';
-        arrow.style.stroke = '#2764e8e9';
-        spanProd.classList.add('inactive-prod-list');
-        spanProd.classList.remove('active-prod-list');
-    }
-}
+  const isActive = spanProd.classList.contains('inactive-prod-list');
 
+  navigation.style.height = isActive ? '530px' : '250px';
+  typeProdMenu.style.display = isActive ? 'flex' : 'none';
+  typeProdMenu.style.clipPath = 'polygon(0 0, 100% 0, 100% 100%, 0 100%)';
+  navigation.style.justifyContent = 'flex-start';
+  dropItemDown.style.marginTop = isActive ? '5px' : '0px';
+  arrow.style.transform = isActive ? 'rotateX(180deg)' : 'rotateX(0deg)';
+  arrow.style.stroke = isActive ? 'red' : '#2764e8e9';
+  spanProd.classList.toggle('active-prod-list', isActive);
+  spanProd.classList.toggle('inactive-prod-list', !isActive);
+};
 
-checkbox.addEventListener('click', function() {
-    if (window.innerWidth <= 1000) {
-      enableNav();
-    }
-  });
-  
-  dropProd.addEventListener('click', function() {
-    if (window.innerWidth <= 1000) {
-      enableProdList();
-    }
-  });
+link.addEventListener('click', closeNav);
+link2.addEventListener('click', closeNav);
+
+checkbox.addEventListener('click', () => {
+  if (window.innerWidth <= 1000) {
+    enableNav();
+  }
+});
+
+dropProd.addEventListener('click', () => {
+  if (window.innerWidth <= 1000) {
+    enableProdList();
+  }
+});
