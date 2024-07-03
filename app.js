ff0cfab7fc3177f1ac7d37378611f33d9778d411
ianrs_19 <ian.rsq@gmail.com>
2024-07-03 17:11:37 -0600

    fjeklfjsdlkfjs

--- app.js
+++
@@ -1,9 +0,0 @@
-const sidebar = document.querySelector('#sidebar');
-
-document.querySelector('#close-sidebar').addEventListener('click', function () {
-    sidebar.style.right = '100%';
-});
-
-document.querySelector('#open-sidebar').addEventListener('click', function () {
-    sidebar.style.right = '0%';
-});
